be1050198a17c9e980fde38d2b01dd3b86135a45
Adam Andrews <andrewsadam07@gmail.com>
2022-11-26 14:16:41 +0000

    Configured tailwind properly

--- features/questions/Question.tsx
+++ features/questions/Question.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Question() {
-	return <div className="bg-red-500  font-bold">This is the question</div>;
+	return <div className="bg-blue-500 font-bold">This is the question</div>;
 }
 
 export default Question;
